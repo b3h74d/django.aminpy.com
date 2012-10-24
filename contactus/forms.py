49ef77624f199d6f348f99e3930c99b391a3783d
Amin Oruji <aminpy@gmail.com>
2012-10-24 20:49:58 +0330

    change project to static html

--- contactus/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms.models import ModelForm
-from contactus.models import Contact
-
-
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
--- contactus/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-import views
-
-
-urlpatterns = patterns('',
-    (r'contactus/$', views.contact_us)
-)
